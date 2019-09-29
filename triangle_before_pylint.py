7544c8b32e5b9e76ed8b3cc0beb4f2ae87fd3beb
Tanvi <thanamsh@steven.edu>
2019-09-29 02:51:40 -0400

    after pylint

--- triangle_before_pylint.py
+++ triangle_before_pylint.py
@@ -4,36 +4,30 @@ CWID: 10445200
 Script: Testing triangle classification
 """
 
-def classify_triangle(a, b, c):
+def classify_triangle(a_value, b_value, c_value):
     """To check if the triangle is valid, equilateral, scalene, isoceles, right"""
-    if a == b == c:
+    if a_value == b_value == c_value:
         print("Equilateral")
         # return 'Equilateral'
-    elif (a == b) or (b == c):
+    elif (a_value == b_value) or (b_value == c_value):
         print("Isoceles")
         # return 'Isoceles'
-    elif ( a + b <= c and b + c <= a and a + c <= b ):
+    elif (a_value+b_value <= c_value and b_value+c_value <= a_value and a_value+c_value <= b_value):
         print("Isoceles")
-        # return 'Not A Triangle'
-    elif any([
-        all([a>b, a>c, (a**2)==((c**2)+(b**2))]),
-        all([c>b, c>a, (c**2)==((a**2)+(b**2))]),
-        all([b>c, b>a, (b**2)==((c**2)+(a**2))]),
-        ]):
+        # return 'Not a Triangle'
+    elif any([all([a_value > b_value, a_value > c_value, (a_value**2) == ((c_value**2)+(b_value**2))]), all([c_value > b_value, c_value > a_value, (c_value**2) == ((a_value**2)+(b_value**2))]), all([b_value > c_value, b_value > a_value, (b_value**2) == ((c_value**2)+(a_value**2))]), ]):
         print("Isoceles")
         # return 'Right'
-    elif ( a + b > c and b + c > a and a + c > b ):
-        if all([a!=c, c!=b, a!=b]):
+    elif (a_value+b_value > c_value and b_value+c_value > a_value and a_value+c_value > b_value):
+        if all([a_value != c_value, c_value != b_value, a_value != b_value]):
             print("Isoceles")
             # return 'Scalene'
     else:
         print("Isoceles")
         # return 'Not A Triangle'
-        
 def main():
-    classify_triangle(1,1,1)
+    """Caling the function"""
+    classify_triangle(1, 1, 1)
 
 main()
-
-
-    
+ 
